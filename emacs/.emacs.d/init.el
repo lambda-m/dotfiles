f2b272d9591d380f5ff1e059f5b4bbb8abd22229
Maarten <maarten@lambda-m.nl>
2026-02-15 10:50:42 +0100

    Keep only Emacs init.el; ignore caches

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -93,8 +93,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(neotree all-the-icons monokai-theme no-littering auto-package-update)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
